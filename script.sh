cf1f8f70e9842396d770101094c03d539975e787
aidynilyas <aidynilyas@gmail.com>
2025-03-21 01:28:19 +0000

    cript.sh added

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+vpcid=$(aws ec2 create-vpc --cidr-block 172.16.0.0/16 --query Vpc.VpcId --output text)
+subnet=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 172.16.1.0/24 --query Subnet.SubnetId --output text)
+networkgateway=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
+aws ec2 attach-internet-gateway --vpc-id $vpcid --internet-gateway-id $networkgateway
