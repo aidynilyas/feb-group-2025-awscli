12826efd8054cadd468bc06d8596b9e9a3800780
aidynilyas <aidynilyas@gmail.com>
2025-03-21 01:35:08 +0000

    another change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # feb-group-2025-awscli
 Kaizen AWS CLI class
+
+2nd change
--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-vpcid=$(aws ec2 create-vpc --cidr-block 172.16.0.0/16 --query Vpc.VpcId --output text)
+vpcid=$(aws ec2 create-vpc --region us-east-2 --cidr-block 172.16.0.0/16 --query Vpc.VpcId --output text)
 subnet=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 172.16.1.0/24 --query Subnet.SubnetId --output text)
 networkgateway=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
 aws ec2 attach-internet-gateway --vpc-id $vpcid --internet-gateway-id $networkgateway
+
+echo "Done!"
